57e56c2c6b20d2d4b0db548daefd441e3d247165
Freeze <92553892+Freeze7x@users.noreply.github.com>
2025-05-13 17:45:49 -0400

    add bongo support

--- osu.Game.Rulesets.Taiko/Mods/TaikoModAlternate.cs
+++ osu.Game.Rulesets.Taiko/Mods/TaikoModAlternate.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Taiko.Objects;
 using osu.Framework.Graphics.Sprites;
+using osu.Game.Configuration;
+using osu.Framework.Bindables;
 
 namespace osu.Game.Rulesets.Taiko.Mods
 {
@@ -19,6 +21,9 @@ namespace osu.Game.Rulesets.Taiko.Mods
         public override double ScoreMultiplier => 1.0;
         public override Type[] IncompatibleMods => [.. base.IncompatibleMods, .. new[] { typeof(TaikoModSingleTap) }];
         public override ModType Type => ModType.Conversion;
+
+        [SettingSource("DDKK/KKDD support", "Adjusts the mod to behave correctly when playing with a color for each side.")]
+        public BindableBool BongoSupport { get; } = new BindableBool();
         private bool? lastActionWasRight = null;
 
         protected override bool CheckValidNewAction(TaikoAction action)
@@ -34,12 +39,53 @@ namespace osu.Game.Rulesets.Taiko.Mods
             bool sideRight = action is TaikoAction.RightRim or TaikoAction.RightCentre;
 
             // Force alternating by disallowing the same side twice in a row
-            if (lastActionWasRight == sideRight)
-            {
-                return false;
-            }
+            if (lastActionWasRight == sideRight) return false;
 
             lastActionWasRight = sideRight;
+            return true;
+        }
+
+        private bool kddkCheck(TaikoAction action)
+        {
+            var nextHitObject = Playfield.HitObjectContainer.AliveObjects.FirstOrDefault(h => h.Result?.HasResult != true)?.HitObject;
+            var previousHitObject = Playfield.HitObjectContainer.AliveObjects.LastOrDefault(h => h.Result?.HasResult == true)?.HitObject;
+
+            // Allow all actions if the next or previous hit object is a strong hit (excluding strong drumrolls).
+            if ((nextHitObject is TaikoStrongableHitObject { IsStrong: true } && nextHitObject is not DrumRoll) ||
+                (previousHitObject is TaikoStrongableHitObject { IsStrong: true } && previousHitObject is not DrumRoll))
+                return true;
+
+            bool sideRight = action is TaikoAction.RightRim or TaikoAction.RightCentre;
+
+            // Force alternating by disallowing the same side twice in a row
+            if (lastActionWasRight == sideRight) return false;
+
+            lastActionWasRight = sideRight;
+            return true;
+        }
+
+        private bool? lastDSideRight = null;
+        private bool? lastKSideRight = null;
+        private bool ddkkCheck(TaikoAction action)
+        {
+            var nextHitObject = Playfield.HitObjectContainer.AliveObjects.FirstOrDefault(h => h.Result?.HasResult != true)?.HitObject;
+            var previousHitObject = Playfield.HitObjectContainer.AliveObjects.LastOrDefault(h => h.Result?.HasResult == true)?.HitObject;
+
+            // Allow all actions if the next or previous hit object is a strong hit (excluding strong drumrolls).
+            if ((nextHitObject is TaikoStrongableHitObject { IsStrong: true } && nextHitObject is not DrumRoll) ||
+                (previousHitObject is TaikoStrongableHitObject { IsStrong: true } && previousHitObject is not DrumRoll))
+                return true;
+
+            bool sideRight = action is TaikoAction.RightRim or TaikoAction.RightCentre;
+            bool colorIsD = action is TaikoAction.LeftCentre or TaikoAction.RightCentre;
+            bool sameSideHit = colorIsD ? (lastDSideRight == sideRight) : (lastKSideRight == sideRight);
+            // Force alternating by disallowing the same side twice in a row
+            if (sameSideHit) return false;
+
+            if (colorIsD)
+                lastDSideRight = sideRight;
+            else
+                lastKSideRight = sideRight;
             return true;
         }
     }
